c9b8f979a8f92b10424cc3f0e5ef1dd5a2e6ea92
Renata Pulz <renata.pulz@raizen.com>
2024-03-10 03:11:14 -0300

    Adicionado tag span pra garantir a ordem na adição de uma tarefa. Adicionado checkbox p/ marcação da conclusão de uma atividade.

--- gerenciador-lista.js
+++ gerenciador-lista.js
@@ -6,16 +6,26 @@ let listaTarefas = [];
 
 insereTarefa.addEventListener('click', function () {
     let novaTarefa = document.createElement('li');
-    novaTarefa.textContent = textoDigitado.value;
+
+    let botaoCheckBox = document.createElement('input');
+    botaoCheckBox.type = 'checkbox';
+    botaoCheckBox.id = 'check-conclusao';
+
+    let textoTarefa = document.createElement('span');
+    textoTarefa.textContent = textoDigitado.value;
+
     let botaoExcluir = document.createElement('i');
     botaoExcluir.classList.add('fa', 'fa-trash');
+
+    novaTarefa.appendChild(botaoCheckBox);
+    novaTarefa.appendChild(textoTarefa);
     novaTarefa.appendChild(botaoExcluir);
 
     botaoExcluir.addEventListener('click', function () {
 
         let index = listaTarefas.indexOf(novaTarefa.textContent); // procura o elemento que eu desejo apagar
         if (index !== -1) { // se der -1 quer dizer que não encontrou nada
-            listaTarefas.splice(index, 1); //removo o elemento do array - 1 é qntos elementos vou apagar
+            listaTarefas.splice(index, 1); //removo o elemento do array, o 1 é qntos elementos vou apagar
         }
         novaTarefa.remove(); // removo do dom
     });
